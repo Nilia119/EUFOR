35ae0663f9e81654cff9ada75d945a31ffbe1b14
Nilia119 <88453817+Nilia119@users.noreply.github.com>
2022-10-03 00:26:29 +0200

    Fixed Typo
    Fixed Typo

--- Addons/Nilia_MedPanel/cfgVehicles.hpp
+++ Addons/Nilia_MedPanel/cfgVehicles.hpp
@@ -4,7 +4,7 @@ class CfgVehicles {
             class ACE_SelfActions {
 				class ACE_Equipment {
                     class openMedPanel {
-                        displayName = MedPanel öffnen;
+                        displayName = MedPanel Oeffnen;
                         condition = "('Nilia_MedPanel' in (uniformItems _player)) || ('Nilia_MedPanel' in (vestItems _player))";
                         statement = createDialog "MedPanel_Dialog";
                         showDisabled = 0;
