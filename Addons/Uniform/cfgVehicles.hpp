f940918b64749e9eb36e0579cff88c143026e82c
Nilia119 <niklas.rieck@gmail.com>
2023-11-01 17:25:41 +0100

    Code Cleanup

--- Addons/Uniform/cfgVehicles.hpp
+++ Addons/Uniform/cfgVehicles.hpp
@@ -25,23 +25,7 @@
 		hiddenSelectionsTextures[] = {"Uniform\Tropen\BW2035_Tropen_Uniform_co.paa"};	// The textures for the selections defined above.
 																								// If empty, no texture is used.
 	};
-/*
-	class BW2035_soldat_Tropen_1: BW2035_BW_Soldier	// Define of a new class, which parameters are inherited from B_Soldier_base_F, with exception of those defined below.
-	{
-		author = "Nilia";			// The name of the author of the asset, which is displayed in the editor.
-		scope = 1;							// 2 = class is available in the editor; 1 = class is unavailable in the editor, but can be accessed via a macro; 0 = class is unavailable (and used for inheritance only).
-		scopeCurator = 0;					// 2 = class is available in Zeus; 0 = class is unavailable in Zeus.
-		scopeArsenal = 2;					// 2 = class is available in the Virtual Arsenal; 0 = class is unavailable in the Virtual Arsenal.
-		identityTypes[] =  {"LanguageENG_F","Head_NATO","G_NATO_default"};		// Identity Types are explained in the Headgear section of this guide.
-		displayName = "Soldat Tropentarn 1";		// The name of the soldier, which is displayed in the editor.
-		model = "\A3\Characters_F\BLUFOR\b_soldier_01.p3d";			// The path to the model this character uses.
-		uniformClass = "W_BW2035_Uniform_Tropen1";							// This links this soldier to a particular uniform. For the details, see below.
-		hiddenSelections[] = {"camo","insignia"};								// List of model selections which can be changed with hiddenSelectionTextures[]
-																	// and hiddenSelectionMaterials[] properties. If empty, model textures are used.
-		hiddenSelectionsTextures[] = {"Uniform\Tropen\BW2035_Tropen_Uniform1_co.paa"};	// The textures for the selections defined above.
-																								// If empty, no texture is used.
-	};
-*/
+
     class BW2035_soldat_Fleck: BW2035_BW_Soldier	// Define of a new class, which parameters are inherited from B_Soldier_base_F, with exception of those defined below.
     {
         author = "Nilia";			// The name of the author of the asset, which is displayed in the editor.
--- Addons/Uniform/cfgWeapons.hpp
+++ Addons/Uniform/cfgWeapons.hpp
@@ -31,27 +31,7 @@
 			mass = 40;
 		};
 	};
-/*
-	class W_BW2035_Uniform_Tropen_1: Uniform_Base
-	{
-		author = "Nilia";
-		scope = 2;
-		scopeArsenal = 2;
-		displayName = "IdN Tropentarn 1";
-		picture = "Uniform\WIP.paa";
-		model = "\A3\Characters_F\Common\Suitpacks\suitpack_original_F.p3d";
-		hiddenSelections[] = {"camo", "insignia"};
-		hiddenSelectionsTextures[] = {"Uniform\Tropen\BW2035_Tropen_Uniform1_co.paa"};
-			
-		class ItemInfo: UniformItem
-		{
-			uniformModel = "-";
-			uniformClass = BW2035_soldat_Tropen_1;
-			containerClass = Supply80;
-			mass = 40;
-		};
-	};
-*/
+
 class W_BW2035_Uniform_Fleck: Uniform_Base
 {
     author = "Nilia";
