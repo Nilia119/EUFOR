f4b0a341a73e1138d0a9a4208ef2a7f9b9af5e85
Nilia119 <niklas.rieck@gmail.com>
2023-11-16 12:15:41 +0100

    Added Basefiles

---
+++ Addons/EUFOR_Objects/cfgPatches.hpp
@@ -0,0 +1,16 @@
+class CfgPatches {
+    class EUFOR_Objects {
+        name = "$STR_EUFOR_Objects_name";
+        units[] = {};
+        requiredAddons[] = {
+            "A3_structures_f_mil"
+        };
+        author = "$STR_EUFOR_Objects_Author";
+        authorUrl = "https://www.eudef.eu";
+        version = 0.0.1;
+        versionStr = "0.0.1";
+        versionAr[] = {0,0,1};
+        weapons[] = {};
+        magazines[] = {};
+    };
+};
