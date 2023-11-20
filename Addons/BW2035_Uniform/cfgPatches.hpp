e7f450254220fbe074258154acfe22dde75daefe
Nilia119 <niklas.rieck@gmail.com>
2023-11-20 19:49:46 +0100

    Rebranded Uniforms

--- Addons/BW2035_Uniform/cfgPatches.hpp
+++ Addons/BW2035_Uniform/cfgPatches.hpp
@@ -2,25 +2,25 @@
 
 class CfgPatches
 {
-	class Nilia_BW2035_Uniform
+	class EUFOR_Uniform
 	{
-        name = "$STR_BW2035_Uniform_name";
+        name = "$STR_EUFOR_Uniform_name";
 		units[]=
 		{
-            "BW2035_BW_Soldier",
-            "BW2035_soldat_Tropen",
-            "BW2035_soldat_Fleck",
-            "BW2035_soldat_Fleck1"
+            "EUFOR_BW_Soldier",
+            "EUFOR_soldat_Tropen",
+            "EUFOR_soldat_Fleck",
+            "EUFOR_soldat_Fleck1"
 		};
 		weapons[]=
 		{
-			"W_BW2035_Uniform_Tropen",
-            "W_BW2035_Uniform_Fleck",
-            "W_BW2035_Uniform_Fleck1"
+			"W_EUFOR_Uniform_Tropen",
+            "W_EUFOR_Uniform_Fleck",
+            "W_EUFOR_Uniform_Fleck1"
 		};
 		requiredVersion=0.1;
 		requiredAddons[]={"A3_Characters_F"};
-        author = "Nilia";
+        author = "$STR_EUFOR_Uniform_Author";
         authorUrl = "https://www.eudef.eu";
         version = 0.0.1;
         versionStr = "0.0.1";
--- Addons/BW2035_Uniform/cfgWeapons.hpp
+++ Addons/BW2035_Uniform/cfgWeapons.hpp
@@ -12,61 +12,61 @@
 //Tatsächliche Uniform als Item hier werden die Einstellungen für die Uniformen getätigt
 
 
-	class W_BW2035_Uniform_Tropen: Uniform_Base
+	class W_EUFOR_Uniform_Tropen: Uniform_Base
 	{
-        author = "$STR_BW2035_Uniform_Author";
+        author = "$STR_EUFOR_Uniform_Author";
 		scope = 2;
 		scopeArsenal = 2;
-		displayName = "$STR_BW2035_Uniform_Tropen";
-		picture = "\BW2035_Uniform\Tropen\Icon_tropen.paa";
+		displayName = "$STR_EUFOR_Uniform_Tropen";
+		picture = "\EUFOR_Uniform\Tropen\Icon_tropen.paa";
 		model = "\A3\Characters_F\Common\Suitpacks\suitpack_original_F.p3d";
 		hiddenSelections[] = {"camo", "insignia"};
-		hiddenSelectionsTextures[] = {"BW2035_Uniform\Tropen\BW2035_Tropen_Uniform_co.paa"};
+		hiddenSelectionsTextures[] = {"EUFOR_Uniform\Tropen\EUFOR_Tropen_Uniform_co.paa"};
 			
 		class ItemInfo: UniformItem
 		{
 			uniformModel = "-";
-			uniformClass = BW2035_soldat_Tropen;
+			uniformClass = EUFOR_soldat_Tropen;
 			containerClass = Supply80;
 			mass = 40;
 		};
 	};
 
-class W_BW2035_Uniform_Fleck: Uniform_Base
+class W_EUFOR_Uniform_Fleck: Uniform_Base
 {
-    author = "$STR_BW2035_Uniform_Author";
+    author = "$STR_EUFOR_Uniform_Author";
     scope = 2;
     scopeArsenal = 2;
-    displayName = "$STR_BW2035_Uniform_Flecktarn";
-    picture = "\BW2035_Uniform\Fleck\Icon_fleck.paa";
+    displayName = "$STR_EUFOR_Uniform_Flecktarn";
+    picture = "\EUFOR_Uniform\Fleck\Icon_fleck.paa";
     model = "\A3\Characters_F\Common\Suitpacks\suitpack_original_F.p3d";
     hiddenSelections[] = {"camo", "insignia"};
-    hiddenSelectionsTextures[] = {"BW2035_Uniform\Fleck\BW2035_Fleck_Uniform_co.paa"};
+    hiddenSelectionsTextures[] = {"EUFOR_Uniform\Fleck\EUFOR_Fleck_Uniform_co.paa"};
 
     class ItemInfo: UniformItem
     {
         uniformModel = "-";
-        uniformClass = BW2035_soldat_Fleck;
+        uniformClass = EUFOR_soldat_Fleck;
         containerClass = Supply80;
         mass = 40;
     };
 };
 
-class W_BW2035_Uniform_Fleck1: Uniform_Base
+class W_EUFOR_Uniform_Fleck1: Uniform_Base
 {
-    author = "$STR_BW2035_Uniform_Author";
+    author = "$STR_EUFOR_Uniform_Author";
     scope = 2;
     scopeArsenal = 2;
-    displayName = "$STR_BW2035_Uniform_Flecktarn_1";
-    picture = "\BW2035_Uniform\Fleck\Icon_fleck.paa";
+    displayName = "$STR_EUFOR_Uniform_Flecktarn_1";
+    picture = "\EUFOR_Uniform\Fleck\Icon_fleck.paa";
     model = "\A3\Characters_F\Common\Suitpacks\suitpack_original_F.p3d";
     hiddenSelections[] = {"camo", "insignia"};
-    hiddenSelectionsTextures[] = {"BW2035_Uniform\Fleck\BW2035_Fleck_Uniform1_co.paa"};
+    hiddenSelectionsTextures[] = {"EUFOR_Uniform\Fleck\EUFOR_Fleck_Uniform1_co.paa"};
 
     class ItemInfo: UniformItem
     {
         uniformModel = "-";
-        uniformClass = BW2035_soldat_Fleck1;
+        uniformClass = EUFOR_soldat_Fleck1;
         containerClass = Supply80;
         mass = 40;
     };
