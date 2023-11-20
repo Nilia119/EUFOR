48575453c4f1b11abf4881b3053d96061c3433e7
Nilia119 <niklas.rieck@gmail.com>
2023-11-20 19:35:00 +0100

    Rebranded Backpacks

--- Addons/Backpack/cfgPatches.hpp
+++ Addons/Backpack/cfgPatches.hpp
@@ -1,11 +1,11 @@
 
 class CfgPatches {
-    class Nilia_MedPanel {
-        name = "$STR_BW2035_Backpack_name";
+    class EUFOR_Backpack {
+        name = "$STR_EUFOR_Backpack_name";
         units[] = {};
         requiredAddons[] = {};
-        author = "$STR_BW2035_Backpack_Author";
-        authorUrl = "https://www.9jgkp.de";
+        author = "$STR_EUFOR_Backpack_Author";
+        authorUrl = "https://www.eudef.eu";
         version = 0.0.1;
         versionStr = "0.0.1";
         versionAr[] = {0,0,1};
--- Addons/Backpack/cfgVehicles.hpp
+++ Addons/Backpack/cfgVehicles.hpp
@@ -26,63 +26,61 @@ class Bag_Base : ReammoBox {
 //    ****************** Carryall ********************
 //  Fleck
 
-class BW2035_Backpack_Fleck_Tortilia : Bag_Base {
-    author = "$STR_BW2035_Backpack_Author";
+class EUFOR_Backpack_Fleck_Tortilia : Bag_Base {
+    author = "$STR_EUFOR_Backpack_Author";
     scope = 2;
     model = "\A3\weapons_f\Ammoboxes\bags\backpack_tortila";
-    displayName = "$STR_BW2035_Backpack_Tortilia-Fleck";
-    picture = "Backpack\Carryall\Fleck\ui\BW2035_Fleck_Backpack_Tortilia_Icon.paa";
-    hiddenSelectionsTextures[]={"Backpack\Carryall\Fleck\BW2035_Fleck_Backpack_Tortilia_co.paa"};
+    displayName = "$STR_EUFOR_Backpack_Tortilia-Fleck";
+    picture = "Backpack\Carryall\Fleck\ui\EUFOR_Fleck_Backpack_Tortilia_Icon.paa";
+    hiddenSelectionsTextures[]={"Backpack\Carryall\Fleck\EUFOR_Fleck_Backpack_Tortilia_co.paa"};
     maximumLoad = 320;
     mass = 60;
 };
 
-class BW2035_Backpack_Fleck_Tortilia_Arzt : Bag_Base {
-    author = "$STR_BW2035_Backpack_Author";
+class EUFOR_Backpack_Fleck_Tortilia_Arzt : Bag_Base {
+    author = "$STR_EUFOR_Backpack_Author";
     scope = 2;
     model = "\A3\weapons_f\Ammoboxes\bags\backpack_tortila";
-    displayName = "$STR_BW2035_Backpack_Tortilia-Fleck_Arzt";
-    picture = "Backpack\Carryall\Fleck\ui\BW2035_Fleck_Backpack_Tortilia-Arzt_Icon.paa";
-    hiddenSelectionsTextures[]={"Backpack\Carryall\Fleck\BW2035_Fleck_Backpack_Tortilia_Arzt_co.paa"};
-    maximumLoad = 320;
+    displayName = "$STR_EUFOR_Backpack_Tortilia-Fleck_Arzt";
+    picture = "Backpack\Carryall\Fleck\ui\EUFOR_Fleck_Backpack_Tortilia-Arzt_Icon.paa";
+    hiddenSelectionsTextures[]={"Backpack\Carryall\Fleck\EUFOR_Fleck_Backpack_Tortilia_Arzt_co.paa"};
     maximumLoad = 820;
     mass = 60;
 };
 
 //  Tropen
 
-class BW2035_Backpack_Tropen_Tortilia : Bag_Base {
-    author = "$STR_BW2035_Backpack_Author";
+class EUFOR_Backpack_Tropen_Tortilia : Bag_Base {
+    author = "$STR_EUFOR_Backpack_Author";
     scope = 2;
     model = "\A3\weapons_f\Ammoboxes\bags\backpack_tortila";
-    displayName = "$STR_BW2035_Backpack_Tortilia-Tropen";
-    picture = "Backpack\Carryall\Tropen\ui\BW2035_Tropen_Backpack_Tortilia_Icon.paa";
-    hiddenSelectionsTextures[]={"Backpack\Carryall\Tropen\BW2035_Tropen_Backpack_Tortilia_co.paa"};
+    displayName = "$STR_EUFOR_Backpack_Tortilia-Tropen";
+    picture = "Backpack\Carryall\Tropen\ui\EUFOR_Tropen_Backpack_Tortilia_Icon.paa";
+    hiddenSelectionsTextures[]={"Backpack\Carryall\Tropen\EUFOR_Tropen_Backpack_Tortilia_co.paa"};
     maximumLoad = 320;
     mass = 60;
 };
 
-class BW2035_Backpack_Tropen_Tortilia_Arzt : Bag_Base {
-    author = "$STR_BW2035_Backpack_Author";
+class EUFOR_Backpack_Tropen_Tortilia_Arzt : Bag_Base {
+    author = "$STR_EUFOR_Backpack_Author";
     scope = 2;
     model = "\A3\weapons_f\Ammoboxes\bags\backpack_tortila";
-    displayName = "$STR_BW2035_Backpack_Tortilia-Tropen_Arzt";
-    picture = "Backpack\Carryall\Tropen\ui\BW2035_Tropen_Backpack_Tortilia-Arzt_Icon.paa";
-    hiddenSelectionsTextures[]={"Backpack\Carryall\Tropen\BW2035_Tropen_Backpack_Tortilia_Arzt_co.paa"};
-    maximumLoad = 320;
+    displayName = "$STR_EUFOR_Backpack_Tortilia-Tropen_Arzt";
+    picture = "Backpack\Carryall\Tropen\ui\EUFOR_Tropen_Backpack_Tortilia-Arzt_Icon.paa";
+    hiddenSelectionsTextures[]={"Backpack\Carryall\Tropen\EUFOR_Tropen_Backpack_Tortilia_Arzt_co.paa"};
     maximumLoad = 820;
     mass = 60;
 };
 
 //  Medic
 
-class BW2035_Backpack_San_Tortilia : Bag_Base {
-    author = "$STR_BW2035_Backpack_Author";
+class EUFOR_Backpack_San_Tortilia : Bag_Base {
+    author = "$STR_EUFOR_Backpack_Author";
     scope = 2;
     model = "\A3\weapons_f\Ammoboxes\bags\backpack_tortila";
-    displayName = "$STR_BW2035_Backpack_Tortilia-San";
-    picture = "Backpack\Carryall\San\ui\BW2035_San_Backpack_Tortilia_Icon.paa";
-    hiddenSelectionsTextures[]={"Backpack\Carryall\San\BW2035_San_Backpack_Tortilia_co.paa"};
+    displayName = "$STR_EUFOR_Backpack_Tortilia-San";
+    picture = "Backpack\Carryall\San\ui\EUFOR_San_Backpack_Tortilia_Icon.paa";
+    hiddenSelectionsTextures[]={"Backpack\Carryall\San\EUFOR_San_Backpack_Tortilia_co.paa"};
     maximumLoad = 820;
     mass = 90;
 };
@@ -90,24 +88,24 @@ class BW2035_Backpack_San_Tortilia : Bag_Base {
 //    ****************** Compact ********************
 //  Fleck
 
-class BW2035_Backpack_Fleck_Compact : Bag_Base {
-    author = "$STR_BW2035_Backpack_Author";
+class EUFOR_Backpack_Fleck_Compact : Bag_Base {
+    author = "$STR_EUFOR_Backpack_Author";
     scope = 2;
     model = "\A3\weapons_f\Ammoboxes\bags\Backpack_Compact";
-    displayName = "$STR_BW2035_Backpack_Compact-Fleck";
-    picture = "Backpack\Compact\Fleck\ui\BW2035_Fleck_Backpack_Compact_Icon.paa";
-    hiddenSelectionsTextures[]={"Backpack\Compact\Fleck\BW2035_Fleck_Backpack_Compact_co.paa"};
+    displayName = "$STR_EUFOR_Backpack_Compact-Fleck";
+    picture = "Backpack\Compact\Fleck\ui\EUFOR_Fleck_Backpack_Compact_Icon.paa";
+    hiddenSelectionsTextures[]={"Backpack\Compact\Fleck\EUFOR_Fleck_Backpack_Compact_co.paa"};
     maximumLoad = 160;
     mass = 20;
 };
 
-class BW2035_Backpack_Fleck_Compact_Rk : Bag_Base {
-    author = "$STR_BW2035_Backpack_Author";
+class EUFOR_Backpack_Fleck_Compact_Rk : Bag_Base {
+    author = "$STR_EUFOR_Backpack_Author";
     scope = 2;
     model = "\A3\weapons_f\Ammoboxes\bags\Backpack_Compact";
-    displayName = "$STR_BW2035_Backpack_Compact-Fleck-Rk";
-    picture = "Backpack\Compact\Fleck\ui\BW2035_Fleck_Backpack_Compact_Rk_Icon.paa";
-    hiddenSelectionsTextures[]={"Backpack\Compact\Fleck\BW2035_Fleck_Backpack_Compact_Rk_co.paa"};
+    displayName = "$STR_EUFOR_Backpack_Compact-Fleck-Rk";
+    picture = "Backpack\Compact\Fleck\ui\EUFOR_Fleck_Backpack_Compact_Rk_Icon.paa";
+    hiddenSelectionsTextures[]={"Backpack\Compact\Fleck\EUFOR_Fleck_Backpack_Compact_Rk_co.paa"};
     maximumLoad = 160;
     mass = 20;
 };
