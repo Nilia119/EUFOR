2ee04e8a03a178c88ac17797e43ab3d084113630
Nilia119 <niklas.rieck@gmail.com>
2026-01-03 22:36:21 +0100

    Added Armored Boonie :D

--- Addons/EUFOR_Helmets/TropenD/W_TropenD.hpp
+++ Addons/EUFOR_Helmets/TropenD/W_TropenD.hpp
@@ -103,6 +103,19 @@ class EUFOR_Helmets_Boonie_TropenD: EUFOR_Helmets_Boonie_Base
     };
 };
 
+class EUFOR_Helmets_Boonie_Spec_TropenD: EUFOR_Helmets_Boonie_Spec_Base
+{
+    author="$STR_EUFOR_Helmets_Author";
+    scope=2;
+    weaponPoolAvailable=1;
+    displayName="$STR_EUFOR_Helmets_TropenD_Boonie_Spec";
+    picture="\EUFOR_Helmets\WIP.paa";
+    hiddenSelectionsTextures[]=
+    {
+        "EUFOR_Helmets\TropenD\EUFOR_Helmets_TropenD_Boonie_co.paa"
+    };
+};
+
 // Military Cap
 
 class EUFOR_Helmets_MilCap_TropenD: EUFOR_Helmets_MilCap_Base
--- Addons/EUFOR_Helmets/cfgWeapons.hpp
+++ Addons/EUFOR_Helmets/cfgWeapons.hpp
@@ -256,6 +256,23 @@ class EUFOR_Helmets_Boonie_Base: EUFOR_Helmets_Beret_Base
     };
 };
 
+class EUFOR_Helmets_Boonie_Spec_Base: EUFOR_Helmets_Boonie_Base
+{
+    class ItemInfo: ItemInfo
+    {
+        mass=10;
+        class HitpointsProtectionInfo
+        {
+            class Head
+            {
+                hitpointName="HitHead";
+                armor=8;
+                passThrough=0.5;
+            };
+        };
+    };
+};
+
 class EUFOR_Helmets_MilCap_Base: EUFOR_Helmets_Beret_Base
 {
     author="$STR_EUFOR_Helmets_Author";
@@ -546,3 +563,7 @@ class EUFOR_Helmets_HelmetB_TI_Base: EUFOR_Helmets_HelmetB_Base
 #include "\EUFOR_Helmets\Tropen\W_Tropen.hpp"
 #include "\EUFOR_Helmets\TropenD\W_TropenD.hpp"
 
+/*
+    SLOTBEZEICHNUNG: https://community.bistudio.com/wiki/getSlotItemName
+
+ */
