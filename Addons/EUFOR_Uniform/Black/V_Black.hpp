006e4576925282b4b566e6ee5a2e55352e359473
Nilia119 <niklas.rieck@gmail.com>
2026-02-02 16:49:35 +0100

    Added Black Digital-Uniform

---
+++ Addons/EUFOR_Uniform/Black/V_Black.hpp
@@ -0,0 +1,116 @@
+/* ------- Normal ------- */
+class EUFOR_Soldat_Black: EUFOR_Soldat
+{
+  author = "$STR_EUFOR_Uniform_Author";
+  scope = 1;
+  scopeCurator = 0;
+  scopeArsenal = 2;
+  identityTypes[] =  {"LanguageENG_F","Head_NATO","G_NATO_default"};
+  displayName = "Soldat Schwarz Digital";
+  model = "\A3\Characters_F\BLUFOR\b_soldier_01.p3d";
+  uniformClass = "W_EUFOR_Uniform_Black";
+  hiddenSelections[] = {"camo","insignia"};
+  hiddenSelectionsTextures[] = {"EUFOR_Uniform\Black\EUFOR_Black_Uniform_co.paa"};
+  
+};
+
+class EUFOR_Soldat_Black1: EUFOR_Soldat
+{
+  author = "$STR_EUFOR_Uniform_Author";
+  scope = 1;
+  scopeCurator = 0;
+  scopeArsenal = 2;
+  identityTypes[] =  {"LanguageENG_F","Head_NATO","G_NATO_default"};
+  displayName = "Soldat Schwarz Digital keine Flagge";
+  model = "\A3\Characters_F\BLUFOR\b_soldier_01.p3d";
+  uniformClass = "W_EUFOR_Uniform_Black1";
+  hiddenSelections[] = {"camo","insignia"};
+  hiddenSelectionsTextures[] = {"EUFOR_Uniform\Black\EUFOR_Black1_Uniform_co.paa"};
+
+};
+/* ------- Ärmel hoch ------- */
+class EUFOR_Soldat_Black_Sleeves: EUFOR_Soldat_Sleeves
+{
+  author = "$STR_EUFOR_Uniform_Author";
+  scope = 1;
+  scopeCurator = 0;
+  scopeArsenal = 2;
+  identityTypes[] =  {"LanguageENG_F","Head_NATO","G_NATO_default"};
+  displayName = "Soldat Schwarz Digital ärmel";
+  model = "\A3\Characters_F\BLUFOR\b_soldier_03.p3d";
+  uniformClass = "W_EUFOR_Uniform_Black_Sleeves";
+  hiddenSelections[] = {"camo","insignia"};
+  hiddenSelectionsTextures[] = {"EUFOR_Uniform\Black\EUFOR_Black_Uniform_co.paa"};
+
+};
+
+class EUFOR_Soldat_Black1_Sleeves: EUFOR_Soldat_Sleeves
+{
+  author = "$STR_EUFOR_Uniform_Author";
+  scope = 1;
+  scopeCurator = 0;
+  scopeArsenal = 2;
+  identityTypes[] =  {"LanguageENG_F","Head_NATO","G_NATO_default"};
+  displayName = "Soldat Schwarz Digital ärmel keine Flagge";
+  model = "\A3\Characters_F\BLUFOR\b_soldier_03.p3d";
+  uniformClass = "W_EUFOR_Uniform_Black1_Sleeves";
+  hiddenSelections[] = {"camo","insignia"};
+  hiddenSelectionsTextures[] = {"EUFOR_Uniform\Black\EUFOR_Black1_Uniform_co.paa"};
+
+};
+/* ------- T-Shirt ------- */
+class EUFOR_Soldat_Black_Shirt: EUFOR_Soldat_Shirt
+{
+  author = "$STR_EUFOR_Uniform_Author";
+  scope = 1;
+  scopeCurator = 0;
+  scopeArsenal = 2;
+  identityTypes[] =  {"LanguageENG_F","Head_NATO","G_NATO_default"};
+  displayName = "Soldat Schwarz Digital T-Shirt";
+  model = "\A3\Characters_F\BLUFOR\b_soldier_02.p3d";
+  uniformClass = "W_EUFOR_Uniform_Black_Shirt";
+  hiddenSelections[] = {"camo","insignia"};
+  hiddenSelectionsTextures[] = {"EUFOR_Uniform\Black\EUFOR_Black_Uniform_co.paa"};
+};
+/* ------- Normal Tarn------- */
+class EUFOR_Soldat_Tarn_Black: EUFOR_Soldat
+{
+  author = "$STR_EUFOR_Uniform_Author";
+  scope = 1;
+  scopeCurator = 0;
+  scopeArsenal = 2;
+  identityTypes[] =  {"LanguageENG_F","Head_NATO","G_NATO_default"};
+  displayName = "Soldat Tarnuniform Schwarz Digital";
+  model = "\A3\Characters_F_Exp\BLUFOR\B_CTRG_Soldier_01_F.p3d";
+  uniformClass = "W_EUFOR_Uniform_Tarn_Black";
+  hiddenSelections[] = {"camo","insignia"};
+  hiddenSelectionsTextures[] = {"EUFOR_Uniform\Black\EUFOR_Black_Tarn_Uniform_co.paa"};
+};
+/* ------- Tarn Ärmel hoch ------- */
+class EUFOR_Soldat_Tarn_Black_Sleeves: EUFOR_Soldat_Sleeves
+{
+  author = "$STR_EUFOR_Uniform_Author";
+  scope = 1;
+  scopeCurator = 0;
+  scopeArsenal = 2;
+  identityTypes[] =  {"LanguageENG_F","Head_NATO","G_NATO_default"};
+  displayName = "Soldat Tarnuniform Schwarz Digital hoch";
+  model = "\A3\Characters_F_Exp\BLUFOR\B_CTRG_Soldier_03_F.p3d";
+  uniformClass = "W_EUFOR_Uniform_Tarn_Black_Sleeves";
+  hiddenSelections[] = {"camo","insignia"};
+  hiddenSelectionsTextures[] = {"EUFOR_Uniform\Black\EUFOR_Black_Tarn_Uniform_co.paa"};
+};
+/* ------- Tarn T-Shirt ------- */
+class EUFOR_Soldat_Tarn_Black_Shirt: EUFOR_Soldat_Shirt
+{
+  author = "$STR_EUFOR_Uniform_Author";
+  scope = 1;
+  scopeCurator = 0;
+  scopeArsenal = 2;
+  identityTypes[] =  {"LanguageENG_F","Head_NATO","G_NATO_default"};
+  displayName = "Soldat Tarnuniform Schwarz Digital T-Shirt";
+  model = "\A3\Characters_F_Exp\BLUFOR\B_CTRG_Soldier_02_F.p3d";
+  uniformClass = "W_EUFOR_Uniform_Tarn_Black_Shirt";
+  hiddenSelections[] = {"camo","insignia"};
+  hiddenSelectionsTextures[] = {"EUFOR_Uniform\Black\EUFOR_Black_Tarn_Uniform_co.paa"};
+};
