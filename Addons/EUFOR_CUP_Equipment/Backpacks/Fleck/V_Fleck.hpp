e26978eb26fc95750bc69e4cc1f105801679c3c2
Nilia119 <niklas.rieck@gmail.com>
2026-02-05 15:31:26 +0100

    Added Icons for all Flecktarn Backpacks

--- Addons/EUFOR_CUP_Equipment/Backpacks/Fleck/V_Fleck.hpp
+++ Addons/EUFOR_CUP_Equipment/Backpacks/Fleck/V_Fleck.hpp
@@ -4,7 +4,7 @@ class EUFOR_Backpacks_Predator_Fleck : EUFOR_Backpacks_Predator_Base {
     author = "$STR_EUFOR_CUP_Equipment_Author";
     scope = 2;
     displayName = "$STR_EUFOR_CUP_Equipment_Backpacks_Predator_Fleck";
-    //picture = "EUFOR_CUP_Equipment\Backpacks\Fleck\ui\EUFOR_Backpacks_Tortilia_Fleck_Icon.paa";
+    picture = "EUFOR_CUP_Equipment\Backpacks\Fleck\ui\EUFOR_Backpacks_Predator_Fleck_Icon.paa";
     hiddenSelectionsTextures[]={"EUFOR_CUP_Equipment\Backpacks\Fleck\EUFOR_Backpacks_Predator_Fleck_co.paa"};
 };
 
@@ -12,6 +12,6 @@ class EUFOR_Backpacks_PredatorRadio_Fleck : EUFOR_Backpacks_PredatorRadio_Base {
     author = "$STR_EUFOR_CUP_Equipment_Author";
     scope = 2;
     displayName = "$STR_EUFOR_CUP_Equipment_Backpacks_PredatorRadio_Fleck";
-    //picture = "EUFOR_Backpacks\Fleck\ui\EUFOR_Backpacks_Tortilia_Fleck_Icon.paa";
+    picture = "EUFOR_CUP_Equipment\Backpacks\Fleck\ui\EUFOR_Backpacks_Predator_Fleck_Radio_Icon.paa";
     hiddenSelectionsTextures[]={"EUFOR_CUP_Equipment\Backpacks\Fleck\EUFOR_Backpacks_Predator_Fleck_co.paa"};
 };
